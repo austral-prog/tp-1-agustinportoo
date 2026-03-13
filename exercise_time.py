0cdceec0ac3802467f00ec6e6bf8d6ecf5cf2a0b
agustinportoo <tanitoporto@gmail.com>
2026-03-13 12:41:18 -0300

    Update exercise_time.py

--- exercise_time.py
+++ exercise_time.py
@@ -8,3 +8,16 @@ def time():
     3. Segundos restantes
     """
     total_segundos = 3665
+
+    minutos = total_segundos / 60
+    minutos_enteros = total_segundos // 60
+    horas = minutos / 60
+    horas_completas = total_segundos // 3600
+    minutos_disponibles = total_segundos - 3600
+    minutos_completos_restantes = minutos_disponibles // 60
+    segundos_restantes =  minutos_disponibles - (minutos_completos_restantes * 60)
+
+
+    print(horas_completas)
+    print(minutos_completos_restantes)
+    print(segundos_restantes)
